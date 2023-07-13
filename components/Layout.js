1206542c99c498adbcca5b659750b967e527f702
Akibrot <akibrotsamuelas@gmail.com>
2023-07-13 13:27:29 +0300

    nav removed

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 function Layout({ children }) {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar  /> */}
       {children}
       {/* <div id="#about">Footer</div> */}
     </div>
